3aca4841b323bca65f5123ea6cb44e2ad8acbff6
ivan.gorobenko <igorobenko@octo.com>
2021-03-12 14:57:38 +0100

    commit

--- test/200-process-order.js
+++ test/200-process-order.js
@@ -1,19 +1,19 @@
 var expect = require('chai').expect;
 var process = require('../lib/process');
 
-describe('process.order(payload, callback)', function () {
-  it('calls back with an empty object', function (done) {
-    process.order({}, function (err, result) {
-      if (err) return done(err);
-      expect(result).to.deep.equal({});
-      done();
+describe('process.order(payload)', function () {
+    it('calls back with an empty object', function (done) {
+        const result = process.order({})
+        expect(result).to.deep.equal({});
     })
-  })
-  it('doit renvoyer le total de 19.03 pour la commande {"prices":[15.99],"quantities":[1],"country":"NL","reduction":"STANDARD"} ', function (done) {
-    process.order({"prices":[27.89, 29.35,],"quantities":[1],"country":"NL","reduction":"STANDARD"}, function (err, result) {
-      if (err) return done(err);
-      expect(result).to.deep.equal({total: 19.03});
-      done();
+    it('doit renvoyer le total de 19.03 pour la commande {"prices":[15.99],"quantities":[1],"country":"NL","reduction":"STANDARD"} ', function () {
+        const result = process.order({
+            "prices": [89.3, 34.99],
+            "quantities": [8, 10],
+            "country": "UK",
+            "reduction": "STANDARD"
+        });
+        console.log(result)
+        expect(result).to.deep.equal({total: 1287.80});
     })
-  })
 })
